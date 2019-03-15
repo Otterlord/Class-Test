9296c8cd8317eb7f8963dd2aca395d1f9b51735b
Otterlord <yourotterlord@gmail.com>
2019-03-15 14:25:37 -0700

    The actual code

---
+++ src/TestClass.java
@@ -0,0 +1,14 @@
+public class TestClass {
+	/*
+	 * This is a class. It contains instance variables and methods.
+	 * It can be instantiated, but the class itself is a bit like DNA and serves as a template.
+	 */
+	
+	private int instanceVariable = -1; // This is an instance variable. Each instance of TestClass has its own instance of instanceVariable.
+	private static int staticVariable = 10; // Static variables are shared across all instances of TestClass.
+	
+	private void testMethod()
+	{
+		System.out.println("!dlrow olleh"); // Calls println() with an argument
+	}
+}
